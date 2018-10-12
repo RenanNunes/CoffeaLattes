9fd0ee30c927cf3c6edc52861e8444cf08b153c8
Renan Nunes <renan_hamada@hotmail.com>
2018-10-12 15:23:05 -0300

    Adicao da pagina para criar Experiencias

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import CriarExperiencia from './views/CriarExperiencia.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/experiencias/criar',
+      name: 'novaExperiencia',
+      component: CriarExperiencia,
     },
   ],
 });
