92ac4726c731b55cbfacd9dd8422e48583d6b89d
Lucas Paiva <lucas.gomes.paiva@usp.br>
2018-12-02 18:23:27 -0200

    Criado empresas.js com lógica para criar, editar e apagar uma empresa Adicionado rotas para métodos de empresa em index.js

---
+++ server/db/empresas.js
@@ -0,0 +1,56 @@
+const Joi = require('joi')
+const db = require('./connection');
+
+const schema = Joi.object().keys({
+    nome: Joi.string().max(75).required().error(new Error('Nome da empresa pode conter até 75 caracteres')),
+    localidades: Joi.array().items(
+        Joi.string().max(100).error(new Error('Cada localidade da empresa pode conter até 100 caracteres'))
+        ).required().error(new Error('A empresa precisa ter ao menos uma localidade')),
+    areaDeAtuacao: Joi.string().max(75).required().error(new Error('Área de atuação pode conter até 75 caracteres')),
+    descricao: Joi.string().max(500).required().error(new Error('A descrição da empresa pode conter menos de 500 caracteres')),
+    site: Joi.string().regex(new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)),
+ /*   notasMedias: Joi.object({
+
+    })
+ */
+});
+
+const empresas = db.get('empresas');
+
+function getAll() {
+    return empresas.find();
+}
+
+function create(novaEmpresa) {
+	//depois de fazer a lógica de criação de empresa, será necessário adicionar a lógica aqui para linkar com a experiência
+	const resultado = Joi.validate(novaEmpresa, schema);
+	if (!resultado.error) {
+		novaEmpresa.dataCriacao = new Date();
+		return empresas.insert(novaEmpresa);
+	} else {
+		const erro = {
+			erro: true,
+			mensagem: resultado.error.message,
+		}
+		return Promise.reject(erro);
+	}
+}
+
+function update(emp){
+    const resultado = Joi.validate(emp, schema);
+    if(!resultado.error) {
+        return empresas.updateOne({_id: emp._id});
+    } else {
+        const erro = {
+            erro: true,
+            mensagem: resultado.error.message,
+        }
+        return Promise.reject(erro);
+    }
+}
+
+function remove(emp){
+    return empresas.deleteOne({_id: emp._id});
+}
+
+module.exports = { getAll, create, update, remove };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const experiencias = require('../db/experiencias.js');
+const empresas = require('../db/empresas.js');
 
 module.exports = function(app) {
 
@@ -7,10 +8,12 @@ module.exports = function(app) {
 			message: 'HomePage'
 		});
 	});
+	
 	app.get('/experiencia', async (req, res) => {
 		const exp = await experiencias.getAll();
 		res.json(exp);
 	})
+
 	app.post('/experiencia/buscar', async (req, res) => {
 		const exp = await experiencias.getAll();
 		res.json(exp);
@@ -19,6 +22,26 @@ module.exports = function(app) {
 	app.post('/experiencia/criar', async (req, res) => {
 		try {
 			const result = await experiencias.create(req.body);
+			res.json(result);
+		} catch (error) {
+			res.status(500);
+			res.json(error);
+		}
+	})
+
+	app.get('/empresa', async (req, res)=> {
+		const emp = await empresas.getAll();
+		res.json(emp);
+	})
+	
+	app.post('/empresa/buscar', async (req, res) => {
+		const emp = await empresas.getAll();
+		res.json(emp);
+	})
+
+	app.post('empresa/criar', async (req, res) => {
+		try {
+			const result = await empresas.create(req.body);
 			res.json(result);
 		} catch (error) {
 			res.status(500);
