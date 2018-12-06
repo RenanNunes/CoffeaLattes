cee526d9d0c3272f0cac3d1a96aa806386ef97ec
Lucas Paiva <lucas.gomes.paiva@usp.br>
2018-12-06 10:36:36 -0200

    Consertar operação remove de empresas Adicionado notasMedias no schema de criação de empresa Update ainda não testado

--- server/db/empresas.js
+++ server/db/empresas.js
@@ -9,10 +9,11 @@ const schema = Joi.object().keys({
     areaDeAtuacao: Joi.string().max(75).required().error(new Error('Área de atuação pode conter até 75 caracteres')),
     descricao: Joi.string().max(500).required().error(new Error('A descrição da empresa pode conter menos de 500 caracteres')),
     site: Joi.string().regex(new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/)),
- /*   notasMedias: Joi.object({
-
-    })
- */
+    notasMedias: Joi.object().keys({
+        ambiente: Joi.number().min(0).max(5).allow(''),
+        aprendizado: Joi.number().min(0).max(5).allow(''),
+        autonomia: Joi.number().min(0).max(5).allow('')
+    }).error(new Error('Notas devem ser de 0 a 5 ou vazias'))
 });
 
 const empresas = db.get('empresas');
@@ -21,12 +22,11 @@ function getAll() {
     return empresas.find();
 }
 
-function create(novaEmpresa) {
+function create(emp) {
 	//depois de fazer a lógica de criação de empresa, será necessário adicionar a lógica aqui para linkar com a experiência
-	const resultado = Joi.validate(novaEmpresa, schema);
+	const resultado = Joi.validate(emp, schema);
 	if (!resultado.error) {
-		novaEmpresa.dataCriacao = new Date();
-		return empresas.insert(novaEmpresa);
+		return empresas.insert(emp);
 	} else {
 		const erro = {
 			erro: true,
@@ -37,12 +37,9 @@ function create(novaEmpresa) {
 }
 
 function update(emp){
-    console.log(emp);
-    const resultado = Joi.validate(emp, schema);
-    console.log("Sera q deu certo?");
+    //const resultado = Joi.validate(emp, schema);
     if(!resultado.error) {
-        console.log("Vou editar");
-        return empresas.update({_id: emp._id});
+        return empresas.updateOne({_id: emp.body["id"]});
     } else {
         const erro = {
             erro: true,
@@ -53,7 +50,7 @@ function update(emp){
 }
 
 function remove(emp){
-    return empresas.delete({_id: emp._id});
+    return empresas.remove(emp);
 }
 
 module.exports = { getAll, create, update, remove };
