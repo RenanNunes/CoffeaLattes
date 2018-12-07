b1c51877149e3b3aab15dd6983f7185d8100c85b
John2509 <jmarcelofo@gmail.com>
2018-12-07 08:24:59 -0200

    Schema de validação feito

--- server/db/usuarios.js
+++ server/db/usuarios.js
@@ -2,8 +2,16 @@ const Joi = require('joi')
 const db = require('./connection');
 
 const schema = Joi.object().keys({
-
+	email : Joi.string().email({ minDomainAtoms: 2 }).require().error(new Error('Email invalido.')),
+	nome : Joi.string().alphanum().min(3).max(30).required().error(new Error('O nome deve conter entre 3 e 30 caracteres alfanumericos.')),
+	senha : Joi.string().required().min(3).max(30).error(new Error('A senha deve conter entre 3 e 30 caracteres.')),
+	curso : Joi.string().required().min(3).max(30).alphanum().error(new Error('O curso deve conter entre 3 e 30 caracteres alfanumericos.'))
 });
+
+const tipoUsuarioEnum = {
+	comum = 0,
+	admin = 1
+}
 
 const usuarios = db.get('usuarios');
 
