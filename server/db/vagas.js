1e00e434aad06fb05ba48e0711e3d2f414dd4558
Renan Nunes <renan_hamada@hotmail.com>
2018-12-09 12:24:47 -0200

    Ajuste numa validacao no Joi e num campo da pagina

--- server/db/vagas.js
+++ server/db/vagas.js
@@ -5,13 +5,13 @@ const schema = Joi.object().keys({
     empresa: Joi.string().max(75).required().error(new Error('Empresa pode conter até 75 caracteres')),
     cargo: Joi.string().max(75).required().error(new Error('Cargo pode conter até 75 caracteres')),
     descricao: Joi.string().required().error(new Error('Necessário inserir descrição')),
-    dataLimite: Joi.date().required().error(new Error('Data limite necessária')),
+    dataLimite: Joi.date().required().error(new Error('Data máxima de inscrição necessária')),
     salario: Joi.number().min(0).precision(2).allow('').error(new Error('O salário deve ser um número maior ou igual a 0')),
     beneficios: Joi.object().keys({
 		VRVA: Joi.number().min(0).precision(2).allow(''),
 		VT: Joi.number().min(0).precision(2).allow(''),
     }).error(new Error('Os Beneficios devem ser valores monetarios válidos ou devem estar vazios')),
-    tipo: Joi.string().valid('semestral', 'quadrimestral', 'ferias', 'trainee', 'outros').required().error(new Error('Ocorreu um erro no Tipo da experiência'))
+    tipo: Joi.string().valid('semestral', 'quadrimestral', 'ferias', 'trainee', 'outro').required().error(new Error('Ocorreu um erro no Tipo da experiência'))
 });
 
 const vagas = db.get('vagas');
