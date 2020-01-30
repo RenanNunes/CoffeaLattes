f0a00e09296a27b9c1915e3acd0918d14963072c
Renan Nunes <renan_hamada@hotmail.com>
2020-01-30 17:08:30 +0000

    Adicao do nome da tab do navegador e ajuste nos links do menu para nao recarregar mais a pagina a cada transicao

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,52 +3,52 @@
 
     <b-navbar toggleable="md" type="dark" variant="secondary">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand href="/">CoffeaLattes</b-navbar-brand>
+      <b-navbar-brand to="/">CoffeaLattes</b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
 
         <b-navbar-nav>
-          <b-nav-item href="#">Sobre</b-nav-item>
+          <b-nav-item to="#">Sobre</b-nav-item>
           <b-nav-item-dropdown right>
             <!-- Using button-content slot -->
             <template slot="button-content">
               <em>Estágios</em>
             </template>
-            <b-dropdown-item href="/experiencias/criar">Adicionar</b-dropdown-item>
-            <b-dropdown-item href="/experiencias/listar/busca">Buscar</b-dropdown-item>
-            <b-dropdown-item href="/experiencias/listar">Listar todos</b-dropdown-item>
+            <b-dropdown-item to="/experiencias/criar">Adicionar</b-dropdown-item>
+            <b-dropdown-item to="/experiencias/listar/busca">Buscar</b-dropdown-item>
+            <b-dropdown-item to="/experiencias/listar">Listar todos</b-dropdown-item>
           </b-nav-item-dropdown>
           <b-nav-item-dropdown right>
             <!-- Using button-content slot -->
             <template slot="button-content">
               <em>Empresas</em>
             </template>
-            <b-dropdown-item href="/empresas/criar">Adicionar</b-dropdown-item>
-            <b-dropdown-item href="/empresas/listar">Listar todas</b-dropdown-item>
+            <b-dropdown-item to="/empresas/criar">Adicionar</b-dropdown-item>
+            <b-dropdown-item to="/empresas/listar">Listar todas</b-dropdown-item>
           </b-nav-item-dropdown>
           <b-nav-item-dropdown right>
             <!-- Using button-content slot -->
             <template slot="button-content">
               <em>Vagas</em>
             </template>
-            <b-dropdown-item href="/vagas/criar">Adicionar</b-dropdown-item>
-            <b-dropdown-item href="/vagas/listar">Listar todos</b-dropdown-item>
+            <b-dropdown-item to="/vagas/criar">Adicionar</b-dropdown-item>
+            <b-dropdown-item to="/vagas/listar">Listar todos</b-dropdown-item>
           </b-nav-item-dropdown>
-          <b-nav-item href="#">Fale conosco</b-nav-item>
+          <b-nav-item to="#">Fale conosco</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item v-if="!this.$cookies.user" right href="/signup">Cadastre-se</b-nav-item>
-          <b-nav-item v-if="!this.$cookies.user" right href="/login">Login</b-nav-item>
+          <b-nav-item v-if="!this.$cookies.user" right to="/signup">Cadastre-se</b-nav-item>
+          <b-nav-item v-if="!this.$cookies.user" right to="/login">Login</b-nav-item>
 
           <b-nav-item-dropdown v-if="this.$cookies.user" right>
             <!-- Using button-content slot -->
             <template slot="button-content">
               <em>Conta</em>
             </template>
-            <b-dropdown-item href="/usuario/detalhar">Perfil</b-dropdown-item>
-            <b-dropdown-item href="/logout">Sair</b-dropdown-item>
+            <b-dropdown-item to="/usuario/detalhar">Perfil</b-dropdown-item>
+            <b-dropdown-item to="/logout">Sair</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
 
